66a243d17f4777a3658c11e0a1ba60e07efa1584
dmteamwork <dmteamwork@proton.me>
2026-05-18 17:42:47 +0300

    Update update_alert.py

--- development/update_alert.py
+++ development/update_alert.py
@@ -4,7 +4,7 @@ import tomllib
 import json
 
 BASE_URL = "https://my-deployment-596fe6.kb.europe-west1.gcp.cloud.es.io"
-api_key = "NHF5UkNwNEJTX3RNbGFPTFc1dEs6aDdmMnpmNUFzNXZWMUlUWEEzdG1jUQ=="
+api_key =  os.environ['ELASTIC_KEY']
 
 headers = {
     "Authorization": f"ApiKey {api_key}",
